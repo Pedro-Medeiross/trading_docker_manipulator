b286699c7be929d386582614cb60a7b31d96ef30
Pedro Medeiros <pedro.contato166@hotmail.com>
2025-06-20 21:58:25 -0300

    updating main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ origins = ["http://localhost:3000", "http://127.0.0.1/8000", "http://localhost:8
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origin,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
